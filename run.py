a28b745a93f8be9996a4d33c881316d74cfbef2f
MarioArocaPaez <mararopae@alum.us.es>
2025-04-30 16:36:23 +0200

    fix db

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
 
-from app import create_app
+from app import create_app, db
 from app.config import config_dict
 
 
 env_name = os.getenv("FLASK_ENV", "development")
 app = create_app(env_name)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
